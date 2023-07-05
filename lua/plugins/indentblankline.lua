b0f14757cff83fcce1b340258452ef0a7e306d73
Kristof Szigethy <szigethykristof@gmail.com>
2023-07-05 12:54:47 +0200

    Add treesitter dependency to indent-blankline

--- lua/plugins/indentblankline.lua
+++ lua/plugins/indentblankline.lua
@@ -1,5 +1,6 @@
 return {
   'lukas-reineke/indent-blankline.nvim',
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
   config = function()
     require('indent_blankline').setup({
       show_current_context = true,
