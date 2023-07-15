391813eeed1d827d61faa6bf9fe91314413e8a94
Kristof Szigethy <szigethykristof@gmail.com>
2023-07-15 15:54:19 +0200

    Open hunk preview automatically with ]c and [c

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -157,13 +157,13 @@ M.gitsigns = function(bufnr)
 
   map('n', ']c', function()
     if vim.wo.diff then return ']c' end
-    vim.schedule(function() gs.next_hunk() end)
+    vim.schedule(function() gs.next_hunk({ preview = true }) end)
     return '<Ignore>'
   end, { buffer = bufnr, expr = true })
 
   map('n', '[c', function()
     if vim.wo.diff then return '[c' end
-    vim.schedule(function() gs.prev_hunk() end)
+    vim.schedule(function() gs.prev_hunk({ preview = true }) end)
     return '<Ignore>'
   end, { buffer = bufnr, expr = true })
 
