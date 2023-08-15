5860d99b71c2f82b576a5139c89df2926e2c653b
Kristof Szigethy <szigethykristof@gmail.com>
2023-08-15 15:35:30 +0200

    Change colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,24 @@
+-- return {
+--   'folke/tokyonight.nvim',
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     require('tokyonight').setup({
+--       -- Possibilities: day, moon, night, storm
+--       style = 'night',
+--       dim_inactive = true
+--     })
+--     vim.cmd.colorscheme('tokyonight')
+--   end
+-- }
+
 return {
-  'folke/tokyonight.nvim',
+  'rose-pine/neovim',
+  name = 'rose-pine',
   lazy = false,
   priority = 1000,
   config = function()
-    require('tokyonight').setup({
-      -- Possibilities: day, moon, night, storm
-      style = 'night',
-      dim_inactive = true
-    })
-    vim.cmd.colorscheme('tokyonight')
+    require('rose-pine').setup({})
+    vim.cmd.colorscheme('rose-pine')
   end
 }
